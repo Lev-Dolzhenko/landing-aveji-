61acbb4ad97e8c4ae87e12f192117602a382f229
Leo <workspace2707@yandex.ru>
2024-03-27 11:17:12 +0300

    created header, hero, about blocks

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    app: "./src/assets/js/index.js",
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    clean: true,
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    static: "./src",
+    compress: true,
+    port: 9000,
+    hot: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(s[ac]ss|css)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.s[ac]ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: "Aveji",
-        template: './src/index.html',
+      title: "Aveji",
+      template: "src/index.html",
     }),
   ],
 };
